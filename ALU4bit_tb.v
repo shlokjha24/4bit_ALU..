300cbe66f3061c912ef88615dd0eec24d8f9aa0b
shlokjha24 <shlokjha2006@gmail.com>
2026-01-22 22:16:52 +0530

    Update ALU4bit_tb.v

--- ALU4bit_tb.v
+++ ALU4bit_tb.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.10.2025 23:52:06
-// Design Name: 
-// Module Name: ALU4bit_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module alu_tb;
